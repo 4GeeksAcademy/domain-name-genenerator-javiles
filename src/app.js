343a6c2f9f1712ed57573fdda3ba9ff4759e25b7
Juan Carlos <jeffycr@gmail.com>
2024-09-27 22:50:29 +0000

    generador de dominios

--- src/app.js
+++ src/app.js
@@ -1,11 +1,28 @@
-/* eslint-disable */
 import "bootstrap";
 import "./style.css";
 
 import "./assets/img/rigo-baby.jpg";
 import "./assets/img/4geeks.ico";
 
-window.onload = function() {
-  //write your code here
-  console.log("Hello Rigo from the console!");
-};
+let pronoun = ["the", "our", "his"];
+let adj = ["great", "big", "amazing"];
+let noun = ["jogger", "racoon", "dog", "merchant", "driver"];
+
+function generateDomains() {
+  let domainList = document.getElementById("domainList");
+
+  for (let i = 0; i < pronoun.length; i++) {
+    for (let j = 0; j < adj.length; j++) {
+      for (let k = 0; k < noun.length; k++) {
+        let domain = pronoun[i] + adj[j] + noun[k] + ".com";
+        let listItem = document.createElement("li");
+        listItem.textContent = domain;
+        domainList.appendChild(listItem);
+      }
+    }
+  }
+}
+
+generateDomains();
+
+console.log("Hello Rigo from the console!");
